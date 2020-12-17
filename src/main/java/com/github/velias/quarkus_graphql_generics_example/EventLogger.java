552f06a51ffd0bb53cea9fc7306698db697c89e9
Vlastimil Elias <velias@redhat.com>
2020-12-17 13:23:20 +0100

    Fixed to Quarkus 1.10.3.Final Added Code to evalueat GraphQL framework produced events

---
+++ src/main/java/com/github/velias/quarkus_graphql_generics_example/EventLogger.java
@@ -0,0 +1,31 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2020 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ */
+package com.github.velias.quarkus_graphql_generics_example;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import org.jboss.logging.Logger;
+
+import io.smallrye.graphql.schema.model.Operation;
+
+/**
+ * Class to examine Events fired by the GraphGQ framework
+ *
+ * @author Vlastimil Elias (velias at redhat dot com)
+ */
+@ApplicationScoped
+public class EventLogger {
+    
+    private static final Logger LOG = Logger.getLogger(EventLogger.class);
+    
+    public void processEvent(@Observes Operation event) {
+        LOG.error(event);
+    }
+
+    
+
+}
